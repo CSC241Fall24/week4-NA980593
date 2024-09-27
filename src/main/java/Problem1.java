d6bce37b7efcb678e1fb25becd369c0881a9e889
Nishchay Arjyal <NA980593@wcupa.edu>
2024-09-27 18:56:19 -0400

    Add problem 1 and 2 solutions

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,6 +4,16 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        ListNode current1 = l1;
+        while (current1.next != null) {
+            current1 = current1.next;
+        }
+        ListNode current2 = l2;
+        while (current2 != null) {
+            current1.next = new ListNode(current2.val);
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+        return l1;
     }
 }
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -4,6 +4,15 @@ public class Problem2 {
         // TODO: Implement the insert method
         // This method should insert a new node with the given value at the specified position
         // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        int pos = 1;
+        ListNode current = head;
+        ListNode newNode = new ListNode(val);
+        while (current.next!=null && pos<position){
+            current = current.next;
+            pos++;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        return head;
     }
 }
